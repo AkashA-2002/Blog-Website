c5a94698fdb22f9f1debb4e226ef3d2fa2dfbaed
Akash A <akash@scube.co>
2023-11-12 03:13:05 +0530

    config updated

--- next.config.js
+++ next.config.js
@@ -1,10 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
   trailingSlash: true,
   images: {
     domains: ["images.ctfassets.net"],
   },
-}
-
-module.exports = nextConfig
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/(.*)",
+        locale: false,
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=10, immutable",
+          },
+        ],
+      },
+    ];
+  },
+};
