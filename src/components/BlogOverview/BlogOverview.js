b4fab180ebd30a7f84bd6bdf47930fef3d5ab7fd
Akash A <akash@scube.co>
2023-11-12 13:15:29 +0530

    filter option added

--- src/components/BlogOverview/BlogOverview.js
+++ src/components/BlogOverview/BlogOverview.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
-import React from "react";
 import Slider from "react-slick";
-import { useState } from "react";
+import React, { useState, Fragment } from "react";
 import { Switch } from "@headlessui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faList } from "@fortawesome/free-solid-svg-icons";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -12,6 +13,7 @@ function classNames(...classes) {
 
 export default function BlogOverview({ data }) {
   const [enabled, setEnabled] = useState(false);
+  const [filterCategory, setFilterCategory] = useState("all");
 
   const latestUpdates = data.slice(0, 3);
   const featuredUpdates = data.slice(3, data?.length);
@@ -25,6 +27,8 @@ export default function BlogOverview({ data }) {
     dots: true,
     pauseOnHover: true,
   };
+  const uniqueCategories = new Set();
+  console.log("filterCategory", filterCategory);
   return (
     <section className="blog-overview-section">
       {/* Slider Section Starts */}
@@ -41,6 +45,7 @@ export default function BlogOverview({ data }) {
             day: "numeric",
             year: "numeric",
           }).format(dateObject);
+          console.log(blog);
           return (
             <div key={index}>
               <div className="blog-latest-card md:grid md:grid-cols-2">
@@ -57,7 +62,9 @@ export default function BlogOverview({ data }) {
                     <h1>{blog?.fields?.blogTitle}</h1>
                   )}
                   {blog?.fields?.miniDescription && (
-                    <p className="description">{blog?.fields?.miniDescription}</p>
+                    <p className="description">
+                      {blog?.fields?.miniDescription}
+                    </p>
                   )}
                   <p className="published-date">{formattedDate}</p>
                 </div>
@@ -74,31 +81,87 @@ export default function BlogOverview({ data }) {
         <div className="container">
           <div className="featured-card-header">
             <h2>Featured Articles</h2>
-            <div className="grid-style">
-              <span>
-                <i class="fa-solid fa-grip"></i>
-              </span>
-              <Switch.Group as="div" className="toggle-option">
-                <Switch
-                  checked={enabled}
-                  onChange={setEnabled}
-                  className={classNames(
-                    enabled ? "bg-indigo-600" : "bg-indigo-800",
-                    "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none"
-                  )}
-                >
-                  <span
-                    aria-hidden="true"
+
+            <div className="left-featured-card-header">
+              <div className="blog-filter-section">
+                <Menu as="div" className="relative inline-block text-left">
+                  <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                    Filter By categories
+                    <ChevronDownIcon
+                      className="-mr-1 h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </Menu.Button>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none filter-option">
+                      <ul>
+                        <li onClick={() => setFilterCategory("all")}>
+                          <div className="py-2">
+                            <Menu.Item>
+                              <span>All</span>
+                            </Menu.Item>
+                          </div>
+                        </li>
+                        {data?.map((category, index) => {
+                          const categories = category?.fields?.categories;
+                          if (categories && !uniqueCategories.has(categories)) {
+                            uniqueCategories.add(categories);
+
+                            return (
+                              <li
+                                key={index}
+                                onClick={() => setFilterCategory(categories)}
+                              >
+                                <div className="py-2">
+                                  <Menu.Item>
+                                    <span>{categories}</span>
+                                  </Menu.Item>
+                                </div>
+                              </li>
+                            );
+                          }
+                        })}
+                      </ul>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
+
+              <div className="grid-style">
+                <span>
+                  <i class="fa-solid fa-grip"></i>
+                </span>
+                <Switch.Group as="div" className="toggle-option">
+                  <Switch
+                    checked={enabled}
+                    onChange={setEnabled}
                     className={classNames(
-                      enabled ? "translate-x-5" : "translate-x-0",
-                      "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+                      enabled ? "bg-indigo-600" : "bg-indigo-800",
+                      "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none"
                     )}
-                  />
-                </Switch>
-              </Switch.Group>
-              <span>
-                <FontAwesomeIcon icon={faList} />
-              </span>
+                  >
+                    <span
+                      aria-hidden="true"
+                      className={classNames(
+                        enabled ? "translate-x-5" : "translate-x-0",
+                        "pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"
+                      )}
+                    />
+                  </Switch>
+                </Switch.Group>
+                <span>
+                  <FontAwesomeIcon icon={faList} />
+                </span>
+              </div>
             </div>
           </div>
           {enabled == true ? (
@@ -118,36 +181,67 @@ export default function BlogOverview({ data }) {
                     day: "numeric",
                     year: "numeric",
                   }).format(dateObject);
-                  return (
-                    <div
-                      key={index}
-                      className="md:grid md:grid-cols-12 md:gap-8 card-wrapper overflow-hidden rounded-2xl border border-gray300 shadow-lg"
-                    >
+                  if (filterCategory == featuredUpdate?.fields?.categories) {
+                    return (
                       <div
-                        className="card-image col-span-6"
-                        style={{
-                          backgroundImage: `url(
-                            https:${
-                              featuredImage ? featuredImage : bannerImage
-                            }
+                        key={index}
+                        className="md:grid md:grid-cols-12 md:gap-8 card-wrapper overflow-hidden rounded-2xl border border-gray300 shadow-lg"
+                      >
+                        <div
+                          className="card-image col-span-6"
+                          style={{
+                            backgroundImage: `url(
+                            https:${featuredImage ? featuredImage : bannerImage}
                           )`,
-                        }}
-                      ></div>
-                      <div className="card-content col-span-6">
-                        {featuredUpdate?.fields?.blogTitle && (
-                          <h4>{featuredUpdate?.fields?.blogTitle}</h4>
-                        )}
-                        {featuredUpdate?.fields?.miniDescription && (
-                          <p className="card-description">
-                            {featuredUpdate?.fields?.miniDescription}
-                          </p>
-                        )}
-                        <div className="published-date">
-                          <p>{formattedDate}</p>
+                          }}
+                        ></div>
+                        <div className="card-content col-span-6">
+                          {featuredUpdate?.fields?.blogTitle && (
+                            <h4>{featuredUpdate?.fields?.blogTitle}</h4>
+                          )}
+                          {featuredUpdate?.fields?.miniDescription && (
+                            <p className="card-description">
+                              {featuredUpdate?.fields?.miniDescription}
+                            </p>
+                          )}
+                          <div className="published-date">
+                            <p>{formattedDate}</p>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  );
+                    );
+                  } else if (filterCategory == "all") {
+                    return (
+                      <div
+                        key={index}
+                        className="md:grid md:grid-cols-12 md:gap-8 card-wrapper overflow-hidden rounded-2xl border border-gray300 shadow-lg"
+                      >
+                        <div
+                          className="card-image col-span-6"
+                          style={{
+                            backgroundImage: `url(
+                        https:${featuredImage ? featuredImage : bannerImage}
+                      )`,
+                          }}
+                        ></div>
+                        <div className="card-content col-span-6">
+                          {featuredUpdate?.fields?.blogTitle && (
+                            <h4>{featuredUpdate?.fields?.blogTitle}</h4>
+                          )}
+                          {featuredUpdate?.fields?.miniDescription && (
+                            <p className="card-description">
+                              {featuredUpdate?.fields?.miniDescription}
+                            </p>
+                          )}
+                          <div className="published-date">
+                            <p>{formattedDate}</p>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  } else {
+                    return null;
+                  }
                 })}
               </div>
             </>
@@ -167,35 +261,70 @@ export default function BlogOverview({ data }) {
                   day: "numeric",
                   year: "numeric",
                 }).format(dateObject);
-                return (
-                  <div
-                    key={index}
-                    className="card-wrapper overflow-hidden rounded-2xl border border-gray300 shadow-lg"
-                  >
-                    <Image
-                      className="card-image"
-                      src={`https:${
-                        featuredImage ? featuredImage : bannerImage
-                      }`}
-                      width={450}
-                      height={350}
-                      alt={featuredUpdate?.fields?.blogTitle}
-                    />
-                    <div className="card-content">
-                      {featuredUpdate?.fields?.blogTitle && (
-                        <h4>{featuredUpdate?.fields?.blogTitle}</h4>
-                      )}
-                      {featuredUpdate?.fields?.miniDescription && (
-                        <p className="card-description">
-                          {featuredUpdate?.fields?.miniDescription}
-                        </p>
-                      )}
-                      <div className="published-date">
-                        <p>{formattedDate}</p>
+                if (filterCategory == featuredUpdate?.fields?.categories) {
+                  console.log(filterCategory == featuredUpdate?.fields?.categories);
+                  return (
+                    <div
+                      key={index}
+                      className="card-wrapper overflow-hidden rounded-2xl border border-gray300 shadow-lg"
+                    >
+                      <Image
+                        className="card-image"
+                        src={`https:${
+                          featuredImage ? featuredImage : bannerImage
+                        }`}
+                        width={450}
+                        height={350}
+                        alt={featuredUpdate?.fields?.blogTitle}
+                      />
+                      <div className="card-content">
+                        {featuredUpdate?.fields?.blogTitle && (
+                          <h4>{featuredUpdate?.fields?.blogTitle}</h4>
+                        )}
+                        {featuredUpdate?.fields?.miniDescription && (
+                          <p className="card-description">
+                            {featuredUpdate?.fields?.miniDescription}
+                          </p>
+                        )}
+                        <div className="published-date">
+                          <p>{formattedDate}</p>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                );
+                  );
+                } else if (filterCategory == "all") {
+                  return (
+                    <div
+                      key={index}
+                      className="card-wrapper overflow-hidden rounded-2xl border border-gray300 shadow-lg"
+                    >
+                      <Image
+                        className="card-image"
+                        src={`https:${
+                          featuredImage ? featuredImage : bannerImage
+                        }`}
+                        width={450}
+                        height={350}
+                        alt={featuredUpdate?.fields?.blogTitle}
+                      />
+                      <div className="card-content">
+                        {featuredUpdate?.fields?.blogTitle && (
+                          <h4>{featuredUpdate?.fields?.blogTitle}</h4>
+                        )}
+                        {featuredUpdate?.fields?.miniDescription && (
+                          <p className="card-description">
+                            {featuredUpdate?.fields?.miniDescription}
+                          </p>
+                        )}
+                        <div className="published-date">
+                          <p>{formattedDate}</p>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                } else {
+                  return null;
+                }
               })}
             </div>
           )}
